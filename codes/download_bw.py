364aa6b35da7867313fc7e00d5e8e110f32417f6
Ramiz Qudsi <qudsiramiz@gmail.com>
2021-11-22 20:15:07 -0500

    deleted:    codes/ACE_sw_flux.pro 	deleted:    codes/Nicks_cusp_finder.pro 	deleted:    codes/download_bw.py 	deleted:    codes/plot_DSCOVR_dxliii.pro 	deleted:    codes/post_file_bw.py 	deleted:    codes/sw_ace_parameters_2hr.png 	deleted:    codes/test.png 	deleted:    codes/test.py 	modified:   figures/Earths_magnetic_field.png 	deleted:    figures/Flux_1329.png 	deleted:    figures/Flux_1330.png 	deleted:    figures/Flux_1331.png 	deleted:    figures/Flux_1332.png 	deleted:    figures/Flux_14-27.png 	deleted:    figures/Flux_14-28.png 	deleted:    figures/Flux_14-36.png 	deleted:    figures/Flux_14-37.png 	deleted:    figures/Flux_14-38.png 	deleted:    figures/Flux_14-40.png 	deleted:    figures/test.ps

--- codes/download_bw.py
+++
@@ -1,5 +0,0 @@
-import urllib
-
-urllib.urlretrieve ("http://services.swpc.noaa.gov/text/ace-swepam.txt", "/Users/bmwalsh/Documents/Research/DXL/ace-swepam.txt")
-
-urllib.urlretrieve ("http://services.swpc.noaa.gov/text/ace-magnetometer.txt", "/Users/bmwalsh/Documents/Research/DXL/ace-magnetometer.txt")
--- codes/post_file_bw.py
+++
@@ -1,6 +0,0 @@
-import ftplib
-session = ftplib.FTP('brianmwalsh.com','USERNAME','PASSWORD')
-file = open('/Users/bmwalsh/Documents/Research/DXL/SW_ACE.png','rb')                  # file to send
-session.storbinary('STOR SW_ACE.png', file)     # send the file
-file.close()                                    # close file and FTP
-session.quit()
