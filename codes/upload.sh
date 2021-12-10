dda46c72142e6bfa2a24a129ab1618deadb5d873
Ramiz Qudsi <qudsiramiz@gmail.com>
2021-12-10 12:30:32 -0500

    new file:   ../.gitignore

--- codes/upload.sh
+++ codes/upload.sh
@@ -1,28 +1,28 @@
 #! /bin/bash
 
-# Lists all the git commands required to push the changes to online repository.
+# lists all the git commands required to push the changes to online repository.
 
 git pull
 
-# Remove any python compiled file.
+# remove any python compiled file.
 
 rm *.pyc
 
-# Remove all the swap files. ( This might also remove recovery files. So 
+# remove all the swap files. ( this might also remove recovery files. so 
 # proceed with caution ).
 
 rm .*swp
 rm .*swp
 rm .*swo
 
-# Add all the files which has been changed.
+# add all the files which has been changed.
 
 git add --a
 
-# Commit all the added files.
+# commit all the added files.
 
 git commit -a
 
-# Push all the files to the GitHub
+# push all the files to the github
 
 git push
