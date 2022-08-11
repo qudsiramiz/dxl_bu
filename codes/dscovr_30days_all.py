93c469367629b417575dc40fa1a8736a0fe96280
Ramiz Qudsi <qudsiramiz@gmail.com>
2022-08-11 13:09:38 -0400

    Update dscovr_30days_all.py

--- codes/dscovr_30days_all.py
+++ codes/dscovr_30days_all.py
@@ -7,6 +7,9 @@ time_code_start = time.time()
 number_of_days = 365
 plot_figures_dsco_30days(number_of_days=number_of_days)
 make_gifs(number_of_days=number_of_days)
+
+# Copy the gif file to google drive
+os.system("cp /mnt/cephadrius/Desktop/git/qudsiramiz.github.io/images/moving_pictures/DSCOVR_30days_hourly_averaged.mp4 /home/cephadrius/google-drive/Studies/Research/bu_research/dxl/figures/vids/")
 #os.system("python dxl_dwld_upld_dscovr_30days_automated.py")
 #os.system("python dxl_mp4_dscovr.py")
 print(f"Time taken: {round(time.time() - time_code_start, 2)} seconds")
